052a68d735d5cec71345a4f73810a20e0e8eaf6c
Thomas GUIRRIEC <thomas@guirriec.fr>
2020-04-23 22:44:39 +0200

    Update Script

--- rfc2307-ssha1-pass.py
+++ rfc2307-ssha1-pass.py
@@ -1,45 +1,27 @@
 #!/usr/bin/python3
 #coding: utf-8
 
-import os
 import hashlib
-from base64 import urlsafe_b64encode as encode
-from base64 import urlsafe_b64decode as decode
+import hmac
+import secrets
+from base64 import b64encode, b64decode
 
-# Description
-# Base64 encoded hash with salt
-# userPassword: {SSHA}MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0
-# Base64 decoded value
-#     SHA1 Hash      Salt
-# --------------------++++
-# 123456789012345678901234
-
-def makeSecret(password):
-    res = False
-    while not res:
-        salt = os.urandom(8)
-        h = hashlib.sha1(password.encode('utf-8'))
-        h.update(salt)
-        secret = "{SSHA}%s" % encode(h.digest() + salt).decode('utf-8')
-        if '-' in secret:
-            res = False
-            continue
-        if '_' in secret:
-            res = False
-            continue
-        res = True
-    return secret
+def makeSecret(cleartext):
+    h = hashlib.sha1(cleartext.encode('utf-8'))
+    salt = secrets.token_bytes(8)
+    h.update(salt)
+    return "{SSHA}%s" % b64encode(h.digest() + salt).decode('utf-8')
 
 def checkPassword(challenge_password, password):
-    challenge_bytes = decode(challenge_password[6:])
+    challenge_bytes = b64decode(challenge_password[6:])
     digest = challenge_bytes[:20]
     salt = challenge_bytes[20:]
     hr = hashlib.sha1(password.encode('utf-8'))
     hr.update(salt)
-    return digest == hr.digest()
+    return hmac.compare_digest(digest, hr.digest())
 
 if __name__ == '__main__':
-     challenge_password = makeSecret('true_password')
-     print(challenge_password)
-     print(checkPassword(challenge_password, 'true_password'))
-     print(checkPassword(challenge_password, 'false_password'))
+    challenge_password = makeSecret('true_password')
+    print(challenge_password)
+    print(checkPassword(challenge_password, 'true_password'))
+    print(checkPassword(challenge_password, 'false_password'))
